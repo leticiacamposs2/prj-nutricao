a9068816a0cba54b0c4c99f326350cd8b2e24570
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2019-09-17 13:21:57 -0300

    Usando Operadores Logicos

--- js/principal.js
+++ js/principal.js
@@ -6,9 +6,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var pesoEhValido = true;
+var alturaEhValida = true;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+if ((peso <= 0) || (peso >= 1000)) {
+    console.log("Peso invalido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
 
-tdImc.textContent = imc;
+if ((altura <= 0) || (altura >= 1000)) {
+    console.log("Altura invalida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (pesoEhValido && alturaEhValida)  {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}

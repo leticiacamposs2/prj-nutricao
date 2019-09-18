481a0f50c507d2dae8192610c067ffd9d768f058
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2019-09-18 19:00:37 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,29 +1,37 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-if ((peso <= 0) || (peso >= 1000)) {
-    console.log("Peso invalido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if ((altura <= 0) || (altura >= 1000)) {
-    console.log("Altura invalida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (pesoEhValido && alturaEhValida)  {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+  if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }

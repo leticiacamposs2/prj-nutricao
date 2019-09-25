039ca4246cd8de222ff906afd3914efc38c06bd5
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2019-09-25 18:46:59 -0300

    Escutando eventos

--- js/principal.js
+++ js/principal.js
@@ -38,6 +38,7 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-titulo.addEventListener("click", function() {
-    console.log("Olá eu fui clicado!");
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function () {
+    console.log("Oi, cliquei no botão.");
 });

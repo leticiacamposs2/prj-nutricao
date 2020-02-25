6dfe3762a9ad971d414a8cd48cb4ed3f95b18b71
Leticia Campos <leticia.campos@totvs.com.br>
2020-02-25 16:55:39 -0300

    capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -8,9 +8,15 @@ botaoAdicionar.addEventListener("click", () => {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", () => {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(paciente => adicionaPacienteNaTabela(paciente));
+        var erroAjax = document.querySelector("#erro-ajax");
+        if ( xhr.status === 200 ) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(paciente => adicionaPacienteNaTabela(paciente));
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
     });
     
     xhr.send();

817922dfe5fd2c2add9012651ad18141130b7574
Leticia Campos <leticia.campos@totvs.com.br>
2020-02-25 14:58:32 -0300

    Filtrando com expressão regular

--- js/filtra.js
+++ js/filtra.js
@@ -1,17 +1,16 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", function() {
+campoFiltro.addEventListener("input", function () {
     var pacientes = document.querySelectorAll(".paciente");
 
     if (this.value.length > 0) {
         pacientes.forEach(paciente => {
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
-            (nome != this.value) ? paciente.classList.add("invisivel") : paciente.classList.remove("invisivel");
+            var expressao = new RegExp(this.value, "i");   
+            (!expressao.test(nome)) ? paciente.classList.add("invisivel") : paciente.classList.remove("invisivel");
         });
     } else {
-        pacientes.forEach(paciente => {
-            paciente.classList.remove("invisivel");
-        });
+        pacientes.forEach(paciente => paciente.classList.remove("invisivel"));
     }
 });
